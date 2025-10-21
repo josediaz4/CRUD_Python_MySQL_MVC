b63c2e1453c4ef2e1813863466e312d9746e95fd
josediaz4 <diaz.jose.mnuel.04@gmail.com>
2025-10-20 21:15:36 -0300

    se agrega funcion Desconectar

--- Conexcion.py
+++ Conexcion.py
@@ -6,7 +6,7 @@ class Conexion:
         try:
 
             conexion = mysql.connector.connect(user='root',
-                                           password='M@nudiaz1',
+                                           password='Tecnica2019',
                                            host='localhost',
                                            database='base',
                                            port='3306')
@@ -17,5 +17,9 @@ class Conexion:
         except mysql.connector.Error as error:  
             print("Error al conectar a la base de datos {}".format(error))
             return conexion
-        
+    
+    def Desconectar (self): 
+         if self.conexion.is_connected():
+             self.conexion.close()
+             print ("Bd desconectada")
  #   ConexionBaseDeDatos()
